2afd0e21a20d8c9ca625da3d52879e8aad4c1f09
tanishaa17 <krati1701@gmail.com>
2022-04-09 01:05:11 +0530

    routes done

--- multipage-form/src/App.jsx
+++ multipage-form/src/App.jsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { UserAddress } from './States/UserAddress';
 import { UserDetails } from './States/UserDetails';
-
+import { Users } from './States/Users';
+import { Home } from './States/Home'
 
 function App() {
   return (
     <div className="App">
-      <UserDetails />
-      <UserAddress />
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route exact path='/registration/one' element={<UserDetails />}></Route>
+        <Route exact path='/registration/two' element={<UserAddress />}></Route>
+        <Route exact path='/users' element={<Users />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ multipage-form/src/States/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export const Home = () => {
+    return (
+        <h1>Home Page</h1>
+    )
+}
